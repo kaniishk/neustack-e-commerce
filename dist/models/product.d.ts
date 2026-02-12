48d86a23fd5e8e51e06fb80e02fa6fc3c6e13dd2
kaniishk <kanishk.411@gmail.com>
2026-02-12 13:33:28 +0530

    Stop tracking node_modules and dist

--- dist/models/product.d.ts
+++
@@ -1,9 +0,0 @@
-export interface Product {
-    id: string;
-    name: string;
-    /**
-     * Price stored in minor units (cents) to avoid floating point errors.
-     */
-    priceCents: number;
-}
-//# sourceMappingURL=product.d.ts.map
--- dist/store/productsStore.d.ts
+++
@@ -1,4 +0,0 @@
-import { Product } from '../models/product';
-export declare function listProducts(): Product[];
-export declare function getProductById(id: string): Product | undefined;
-//# sourceMappingURL=productsStore.d.ts.map
